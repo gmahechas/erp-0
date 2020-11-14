b8fcc28cc1c1cde40dce1e6aa0a384d30eb30fd8
gmahechas <gmahechas@ingenieros.com>
2020-11-14 11:14:13 -0500

    feat: setup https service

--- packages/api-gateway/api-gateway-nestjs/src/main.ts
+++ packages/api-gateway/api-gateway-nestjs/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import * as fs from 'fs';
 
 import * as helmet from 'helmet';
 import * as csurf from 'csurf';
@@ -10,7 +11,12 @@ import config from '@api-gateway-nestjs/utils/config';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, { cors: true });
+  const httpsOptions = {
+    key: fs.readFileSync(__dirname + '/../src/utils/ssl/key.pem'),
+    cert: fs.readFileSync(__dirname + '/../src/utils/ssl/cert.pem'),
+    passphrase: 'Analu'
+  }
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, { httpsOptions, cors: true });
   app.set('trust proxy', 1);
   //app.use(helmet());
   //app.use(csurf());
