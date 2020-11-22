8873db3e84e5d0c02430b9d6e02ba132776e3da3
gmahechas <gmahechas@ingenieros.com>
2020-11-22 12:34:40 -0500

    refactor: redtur user controller

--- packages/auth/auth-nestjs/src/modules/user/server/grpc/user-grpc.controller.ts
+++ packages/auth/auth-nestjs/src/modules/user/server/grpc/user-grpc.controller.ts
@@ -1,15 +1,6 @@
 import { Controller } from '@nestjs/common';
-import { GrpcMethod } from '@nestjs/microservices';
 
-import { Observable, of } from 'rxjs';
-
-import {
-  BaseGrpcController,
-  IEntityOne,
-  userJoiSchema,
-  ISigninRequest,
-  ISigninResponse,
-} from '@gmahechas/common-nestjs';
+import { BaseGrpcController, userJoiSchema } from '@gmahechas/common-nestjs';
 
 import { UserMongodbService } from '@auth/modules/user/client/mongodb/user-mongodb.service';
 
@@ -20,14 +11,5 @@ export class UserGrpcController extends BaseGrpcController(
 ) {
   constructor(private readonly userMongodbService: UserMongodbService) {
     super(userMongodbService);
-  }
-
-  @GrpcMethod('UserService')
-  singin(
-    data: IEntityOne<ISigninRequest>,
-  ): Observable<IEntityOne<ISigninResponse>> {
-    const user = this.userMongodbService.searchOneByUsername(data);
-    console.log(user);
-    return of({ entity: { successAuthUser: false } });
   }
 }
